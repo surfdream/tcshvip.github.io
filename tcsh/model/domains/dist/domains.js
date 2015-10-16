8adae143a681757d901f4834893fe71bdf929292
sunny <lu_yan_liang@163.com>
2015-10-16 09:45:51 +0800

    add domain.js

---
+++ tcsh/model/domains/dist/domains.js
@@ -0,0 +1,32 @@
+/*
+domains
+*/
+define(function (require, exports, module) {
+    "require:nomunge,exports:nomunge,module:nomunge";
+    exports.a = 'http://s.tcsh.me';
+    exports.www = 'http://www.tcsh.me';
+    exports.item = 'http://item.tcsh.me';
+    exports.member = 'http://member.tcsh.me';
+    exports.sell = 'http://sell.tcsh.me';
+    exports.seller = 'http://seller.tcsh.me';
+    exports.order = 'http://order.tcsh.me';
+    exports.api = 'http://api.tcsh.me';
+    exports.api2 = 'http://api2.tcsh.me';
+    exports.cart = 'http://cart.tcsh.me';
+    exports.account = 'http://www.tcsh.me';
+    exports.click = 'http://click.tcsh.me';
+    exports.sb = 'http://sb.tcsh.me';
+    exports.operator = 'http://operator.tcsh.me';
+    exports.shop = 'http://shop.tcsh.me';
+    exports.sns = 'http://sns.tcsh.me';
+    exports.commune = 'http://commune.tcsh.me';
+    exports.profile = 'http://profile.tcsh.me';
+    exports.l = 'http://l.tcsh.me';
+    exports.i = 'http://i.tcsh.me';
+    exports.z = 'http://z.tcsh.me';
+    exports.s = 'http://s.tcsh.me';
+    exports.x = 'http://x.tcsh.me';
+    exports.y = 'http://y.tcsh.me';
+    exports.j = 'http://j.tcsh.me';
+    exports.m = 'http://m.tcsh.me';
+});
